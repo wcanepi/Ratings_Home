f838f235f1bb54a0f0941cc4ab93b1778a41bbb3
Hackbright Student <info@hackbrightacademy.com>
2014-10-23 17:34:41 -0700

    Ratings project Day 1. Loading data in progress

--- model.py
+++ model.py
@@ -1,13 +1,49 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, DateTime
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
 
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable = True)
+    password = Column(String(64), nullable = True)
+    age = Column(Integer, nullable = True)
+    zipcode = Column(String(15), nullable = True)
+
+class Movies(Base):
+    __tablename__ = "movies"
+
+    movie_id = Column(Integer, primary_key = True)
+    title = Column(String(120), nullable = False)
+    release_date = Column(DateTime)
+    imdb_url = Column(String(64))
+
+class Ratings(Base):
+    __tablename__ = "ratings"
+    rating_id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, nullable = False)
+    movie_id = Column(Integer, nullable = False)
+    rating = Column(Integer, nullable = False)
 
 ### End class declarations
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo = True)
+    Session = sessionmaker(bind=ENGINE)
+
+
+    return Session()
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -3,19 +3,40 @@ import csv
 
 def load_users(session):
     # use u.user
-    pass
+    with open('seed_data/shortuser.csv', 'rb') as csvfile:
+        userreader = csv.reader(csvfile, delimiter = "|", quotechar='|')
+        for row in userreader:
+            user = model.User(email=" ", password=" ", age=row[1], zipcode=row[4])
+            session.add(user)
+            session.commit()
+
 
 def load_movies(session):
     # use u.item
-    pass
+    with open('seed_data/shortitem.csv', 'rb') as csvfile:
+        userreader = csv.reader(csvfile, delimiter = "|", quotechar='|')
+        for row in userreader:
+            movie_title = row[1]
+            movie_title = movie_title.strip(movie_title[-6:len(movie_title)])
+            print movie_title
+            #movie = model.Movies(title=row[1], release_date=row[2], imdb_url=row[4])
+            # session.add(movie)
+            # session.commit()
 
 def load_ratings(session):
     # use u.data
-    pass
+    with open('seed_data/shortdata.csv', 'rb') as csvfile:
+        userreader = csv.reader(csvfile, delimiter = " ", quotechar='|')
+        for row in userreader:
+            print row
+            user = model.User(email=" ", password=" ", age=row[1], zipcode=row[4])
+            session.add(user)
+            session.commit()
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    # load_users(session)
+      load_movies(session)
 
 if __name__ == "__main__":
     s= model.connect()
