a5bca0dc5d286745feb1ae9c2a0717d8795343d3
Hackbright Student <info@hackbrightacademy.com>
2014-10-28 15:36:47 -0700

    Working login and create user routes.

--- judgement.py
+++ judgement.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template, redirect, request
-import model
+from flask import Flask, render_template, redirect, request, flash, session
+import model, json
 app = Flask(__name__)
+app.secret_key = '\xf5!\x07!qj\xa4\x08\xc6\xf8\n\x8a\x95m\xe2\x04g\xbb\x98|U\xa2f\x03'
 
 
 @app.route("/")
 def index():
     user_list = model.session.query(model.User).limit(5).all()
     return render_template("user_list.html", user_list=user_list)
+
+@app.route("/createuser", methods=["GET"])
+def show_createuser():
+    return render_template("create_user.html")
+
+@app.route("/createuser", methods=["POST"])
+def createuser():
+    user = model.User(email=request.form['email'], 
+        password=request.form['password'], age=request.form['age'], 
+        zipcode=request.form['zipcode'])
+    print request.form['email']
+    r = model.session.query(model.User).filter_by(email = request.form['email']).all()
+    print type(r)
+    print r[0].email
+    if len(r) > 0:
+        print "Hello"
+        return redirect("/login")
+    model.session.add(user)
+    model.session.commit()
+    print "User has been added"
+    return "User has been added"
 
 
 @app.route("/login", methods=["GET"])
@@ -16,9 +38,44 @@ def show_login():
 
 @app.route("/login", methods=["POST"])
 def process_login():
+    #user = model.User()
+    email = request.form['email']
+    r = model.session.query(model.User).filter_by(email = email).all()
+    
+    if len(r)==0:
+        #user does not exist in database
+        return redirect("/createuser")
+    else:
+        user = r[0]
+        if user.password != request.form['password']:
+            #passwords do not match/ Want to Flash method.
+            print("Your password does not match")
+            #message = 'Passwords do not match, please try again'
+            #flash(message)
+            return redirect("/login")        
+        else:
+            #passwords match store in session
+            session['userid'] = user.id
+            print session['userid']
+            return redirect("/")
+
+# @app.route("/viewusers", methods=["POST"])
+# def process_login():
+#     #user = model.User(
+
+
+@app.route("/sessionclear")
+def clear_session():
+    session.clear()
+    return "Clearing"
+            
+
+    
+
+
     """TODO: Receive the user's login credentials located in the 'request.form'
     dictionary, look up the user, and store them in the session."""
-    return "Oops! This needs to be implemented"
+
 
 if __name__ == "__main__":
     app.run(debug = True)
